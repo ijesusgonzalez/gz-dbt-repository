918b303ebfdac234f2b36b0b033e547b9e5440f1
Jesús <jesusgr.99@hotmail.com>
2023-10-31 16:53:56 +0000

    init

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,7 @@
 SELECT
 s.*
 , p.purchase_price
+, ROUND(purchase_price * quantity,2) AS purchase_cost
+, ROUND(revenue - (purchase_price * quantity),2) AS margin
 FROM {{ ref('stg_raw__sales')}} AS s
 JOIN {{ ref('stg_raw__product')}} AS p ON s.products_id = p.products_id
