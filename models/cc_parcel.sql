015914647e5dd8ff54d852b1ddd5012711588adf
Jesús <jesusgr.99@hotmail.com>
2023-11-01 23:05:30 +0000

    init

--- models/cc_parcel.sql
+++ models/cc_parcel.sql
@@ -2,6 +2,7 @@ WITH nb_model AS (
 
 SELECT
 parcel_id
+,SUM(quantity) AS quantity
 ,COUNT(DISTINCT model_name) AS nb_model
 FROM {{ref('stg_raw__product')}}
 GROUP BY parcel_id)
@@ -22,9 +23,11 @@ END AS status
 ,DATE_DIFF(date_shipping, date_purchase, day) AS expedition_time
 ,DATE_DIFF(date_delivery, date_shipping, day) AS transport_time
 ,DATE_DIFF(date_delivery, date_purchase, day) AS delivery_time
+,IF(date_delivery IS null, null, IF (DATE_DIFF(date_delivery, date_purchase, day)>5,1,0)) AS delay
+,m.quantity
 ,m.nb_model
 FROM {{ref('stg_raw__parcel')}} as p
 JOIN nb_model as m
 USING (parcel_id)
 -- GROUP BY parcel_id
-ORDER BY parcel_id
+ORDER BY parcel_id DESC
--- models/cc_parcel_products.sql
+++ models/cc_parcel_products.sql
@@ -1,6 +1,5 @@
 SELECT
 c.*
-, p.model_name
-, p.quantity
 FROM {{ref('cc_parcel')}} c
 JOIN {{ref('stg_raw__product')}} as p USING (parcel_id)
+ORDER BY parcel_id DESC 
