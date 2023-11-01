8d9079eff122764a3be034a6b79780631dbf765f
Jesús <jesusgr.99@hotmail.com>
2023-11-01 15:47:17 +0000

    init

--- models/cc_parcel.sql
+++ models/cc_parcel.sql
@@ -1,5 +1,8 @@
 SELECT
-pr.*
-FROM {{ref(stg_raw__product)}} as pr
-JOIN {{ref(stg_raw__parcel)}} as pa
-USING parcel_id
+pa.*
+, pr.model_name
+, pr.quantity
+FROM {{ref('stg_raw__parcel')}} as pa
+JOIN {{ref('stg_raw__product')}} as pr
+USING (parcel_id)
+ORDER BY parcel_id
